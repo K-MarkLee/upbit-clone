08ad65e3822fec7d9deda944916cb9681e0caefd
K-MarkLee <seungyeol6825@gmail.com>
2026-02-09 22:40:20 +0900

    initiate project

---
+++ src/main/java/com/project/upbit_clone/UpbitCloneApplication.java
@@ -0,0 +1,13 @@
+package com.project.upbit_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbitCloneApplication.class, args);
+	}
+
+}
